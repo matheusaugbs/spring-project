1ba3f7952b8ee80d4463f1fee8cbc3e7dde6aeac
Matheus Augusto <matheus_silva.barreto@hotmail.com.br>
2020-07-12 12:55:26 -0300

    Project created

---
+++ src/test/java/com/matheusaugbs/springproject/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusaugbs.springproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
