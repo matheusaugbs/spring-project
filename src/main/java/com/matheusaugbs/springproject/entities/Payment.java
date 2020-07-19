1f1ce094b1c9b0c993b84dc5ba5298af797b71de
Matheus Augusto <matheus_silva.barreto@hotmail.com.br>
2020-07-18 23:57:52 -0300

    Subtotal & Total methods

--- src/main/java/com/matheusaugbs/springproject/entities/Payment.java
+++ src/main/java/com/matheusaugbs/springproject/entities/Payment.java
@@ -1,5 +1,7 @@
 package com.matheusaugbs.springproject.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +21,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     @MapsId
     @OneToOne
     private Order order;
