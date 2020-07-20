c218671927202d5482b64321b830073f3ee07e49
Matheus Augusto <matheus_silva.barreto@hotmail.com.br>
2020-07-20 14:30:05 -0300

    User delete

--- src/main/java/com/matheusaugbs/springproject/resources/UserResource.java
+++ src/main/java/com/matheusaugbs/springproject/resources/UserResource.java
@@ -4,6 +4,7 @@ import com.matheusaugbs.springproject.entities.User;
 import com.matheusaugbs.springproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +38,11 @@ public class UserResource {
         user = service.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
